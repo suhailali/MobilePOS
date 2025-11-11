fce8020f8b3c51b6d56b3982526d95075d01a46f
suhailali <suhailali.offl@gmail.com>
2025-11-11 07:19:03 +0530

    fix ui bottom sheet

--- app/src/main/java/com/skegworks/mobilepos/ui/component/TextFieldBottomSheet.kt
+++ app/src/main/java/com/skegworks/mobilepos/ui/component/TextFieldBottomSheet.kt
@@ -1,11 +1,9 @@
 package com.skegworks.mobilepos.ui.component
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
@@ -14,6 +12,7 @@ import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.skegworks.mobilepos.utils.Dimens
@@ -23,7 +22,7 @@ import com.skegworks.mobilepos.utils.Dimens
 fun TextFieldBottomSheet(
     label: String,
     onItemSelected: (String) -> Unit,
-    onDismiss : () -> Unit
+    onDismiss: () -> Unit
 ) {
     // Implementation of a simple bottom sheet that displays a text field
     val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
@@ -32,13 +31,17 @@ fun TextFieldBottomSheet(
         onDismissRequest = onDismiss,
         sheetState = sheetState
     ) {
-        Column {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             SimpleTextField(
                 textState = state.value,
                 label = label
             ) {
                 state.value = it
             }
+            Spacer(modifier = Modifier.padding(Dimens.MEDIUM_PADDING.dp))
             Button(onClick = {
                 onItemSelected(
                     state.value
@@ -46,6 +49,7 @@ fun TextFieldBottomSheet(
             }) {
                 Text("Done")
             }
+            Spacer(modifier = Modifier.padding(Dimens.LARGE_PADDING.dp))
         }
     }
 }
