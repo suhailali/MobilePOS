6e46d43dd22c7c0f578b5dfb8a6fdd395607223c
suhailali <suhailali.offl@gmail.com>
2025-11-13 22:44:47 +0530

    update constant class structure

--- app/src/main/java/com/skegworks/mobilepos/utils/Constants.kt
+++ app/src/main/java/com/skegworks/mobilepos/utils/Constants.kt
@@ -1,17 +1,19 @@
 package com.skegworks.mobilepos.utils
 
 class Constants {
-    companion object FirebaseDocument {
-        const val APP_SETTINGS = "app_settings"
-        const val BUSINESS = "business"
-        const val CATEGORIES = "categories"
-        const val PRODUCTS = "products"
-        const val CUSTOMERS = "customers"
-        const val VENDORS = "vendors"
-        const val INVOICES = "invoices"
-        const val INVOICE_ITEMS = "invoice_items"
-        const val COUPONS = "coupons"
-        const val CASH_COUNTER = "cash_counter"
+    class FirebaseDocument {
+        companion object {
+            const val APP_SETTINGS = "app_settings"
+            const val BUSINESS = "business"
+            const val CATEGORIES = "categories"
+            const val PRODUCTS = "products"
+            const val CUSTOMERS = "customers"
+            const val VENDORS = "vendors"
+            const val INVOICES = "invoices"
+            const val INVOICE_ITEMS = "invoice_items"
+            const val COUPONS = "coupons"
+            const val CASH_COUNTER = "cash_counter"
+        }
     }
 
     class DateFormat {
