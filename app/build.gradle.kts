d4dfab12c081dfc08998e778c4e4baca12a1a778
suhailali <suhailali.offl@gmail.com>
2025-11-18 22:33:59 +0530

    add google font dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.ui.text.google.fonts)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.material.icon)
     implementation(libs.androidx.appcompat)
