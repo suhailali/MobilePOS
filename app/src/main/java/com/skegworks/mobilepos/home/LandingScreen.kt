42c555c0182a411a4e760eb1b2b0d80a53fa5f1a
suhailali <suhailali.offl@gmail.com>
2025-11-18 22:34:33 +0530

    update theme

--- app/src/main/java/com/skegworks/mobilepos/home/LandingScreen.kt
+++ app/src/main/java/com/skegworks/mobilepos/home/LandingScreen.kt
@@ -4,19 +4,24 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.skegworks.mobilepos.ui.theme.MobilePOSTheme
+import com.skegworks.mobilepos.utils.Dimens
 
 @Composable
 fun LandingScreen(
@@ -26,21 +31,20 @@ fun LandingScreen(
 ) {
     val state = viewModel.state.collectAsState()
     LazyVerticalGrid(
-        modifier = modifier,
+        modifier = modifier.padding(Dimens.LARGE_PADDING.dp),
         columns = GridCells.Fixed(3),
         horizontalArrangement = Arrangement.spacedBy(16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp),
     ) {
         items(state.value.features) { item ->
-            Box(
+            Box(contentAlignment = Alignment.Center,
                 modifier = Modifier
-                    .padding(12.dp)
-                    .size(32.dp)
+                    .clip(RoundedCornerShape(20.dp))
                     .background(MaterialTheme.colorScheme.secondary)
                     .clickable {
                         onClick(item)
                     }) {
-                Text(text = item)
+                Text(text = item, modifier = Modifier.padding(vertical = Dimens.MEDIUM_PADDING.dp))
             }
         }
     }
