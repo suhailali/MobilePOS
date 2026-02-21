0d72faf71d398e08f980338ef7b922695dd2b909
suhailali <suhailali.offl@gmail.com>
2026-02-21 17:03:15 +0530

    fix product quantity update after sale

--- app/src/main/java/com/skegworks/mobilepos/pos/UpdateInventoryAfterSaleUseCaseImpl.kt
+++ app/src/main/java/com/skegworks/mobilepos/pos/UpdateInventoryAfterSaleUseCaseImpl.kt
@@ -7,7 +7,7 @@ import com.skegworks.mobilepos.product.SyncProductUseCase
 class UpdateInventoryAfterSaleUseCaseImpl(private val productRepository: ProductRepository, private val syncProductUseCase: SyncProductUseCase): UpdateInventoryAfterSaleUseCase {
     override suspend fun invoke(invoice: Invoice) {
         invoice.items.forEach { item ->
-            productRepository.increaseProductQuantity(item.productId, item.quantity)
+            productRepository.decreaseProductQuantity(item.productId, item.quantity)
             productRepository.getProductById(item.productId)?.let {
                 syncProductUseCase.invoke(it)
             }
